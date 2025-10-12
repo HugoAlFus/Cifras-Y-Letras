80e6d16b2f6c4d35a9678766c15ea01e61a3a997
HugoAlm-DAM <hugoalfus@gmail.com>
2025-10-12 09:00:51 +0200

    validación completada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,38 @@
-let arrayInputs;
+const regex = /^[A-ZÑ]$/;
+const mensajeError = document.querySelector(".error-message");
+
+let arrayInputs = document.querySelectorAll(".inputs input");
 let botonEnviar = document.getElementById("enviar");
 
 function enviarLetras() {
-  arrayInputs = document.querySelectorAll(".inputs input");
+  let todosValidos = true;
 
-  arrayInputs.forEach((input, index) => {
-    console.log(`Input ${index + 1}:`, input.value);
+  arrayInputs.forEach((input) => {
+    const esValido = validarInput(input);
+    if (!esValido) todosValidos = false;
   });
+
+  if (todosValidos) {
+    console.log("✅ Todos los valores son correctos");
+    mensajeError.textContent = "";
+  } else {
+    console.log("❌ Hay errores en algunos campos");
+    mensajeError.textContent =
+      "Hay valores inválidos (solo se admiten letras, acentos tampoco) o campos vacíos";
+  }
 }
 
-botonEnviar.addEventListener("click",enviarLetras);
+function validarInput(input) {
+  const valor = input.value.toUpperCase();
+  input.value = valor;
+
+  if (valor === "" || !regex.test(valor)) {
+    input.classList.add("error");
+    return false;
+  } else {
+    input.classList.remove("error");
+    return true;
+  }
+}
+
+botonEnviar.addEventListener("click", enviarLetras);
